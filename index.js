a60029dc66f89ab7f1e281e1dbddf1aff0be18b8
boreal <raspyatol@linuxmail.org>
2023-10-28 19:55:17 -0700

    added project links

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const userID = "754484719306932244"
-let data = null
+const userName = "bor-real"
+
+let lanyardData = null
+let githubData = null
+
+// i suck at js
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
@@ -12,37 +17,71 @@ const statusColors = {
     "offline": "darkslategrey"
 }
 
-function setDOMValues() {
+function setLanyardDOMValues() {
     const statusText = document.getElementById("status-text")
     const profilePFP = document.getElementById("status-pfp")
 
-    const discordStatus = data.data.discord_status
+    const discordStatus = lanyardData.data.discord_status
     profilePFP.style.borderColor = statusColors[discordStatus]
 
-    if (data.data.activities && data.data.activities.length > 0) {
-        const state = data.data.activities[0].state
+    if (lanyardData.data.activities && lanyardData.data.activities.length > 0) {
+        const state = lanyardData.data.activities[0].state
         if (typeof state !== 'undefined') {
             statusText.textContent = "Status: " + state
         }
     } else {
-        statusText.textContent = "Status: " + capitalizeFirstLetter(data.data.discord_status)
+        statusText.textContent = "Status: " + capitalizeFirstLetter(lanyardData.data.discord_status)
     }
 }
 
-async function main() {
+function setGithubDOMValues() {
+    const projectDiv = document.getElementById("projects");
+
+    for (const project of githubData) {
+        if (project.fork == true) {
+            continue;
+        }
+
+        const projectElement = document.createElement("div", { is: "expanding-list" });
+
+        projectElement.innerHTML = `
+            <a class="project" href="${project.html_url}">${project.name}</a>
+        `;
+
+        projectDiv.appendChild(projectElement);
+    }
+}
+
+async function getLanyard() {
     try {
         const response = await fetch(`https://api.lanyard.rest/v1/users/${userID}`)
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`)
         }
 
-        data = await response.json()
+        lanyardData = await response.json()
 
-        setDOMValues()
+        setLanyardDOMValues()
     } 
     catch (error) {
         console.error("Error:", error)
     }
 }
 
-main()
+async function getGithubProjects() {
+    try {
+        const response = await fetch(`https://api.github.com/users/${userName}/repos`)
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        githubData = await response.json()
+        setGithubDOMValues()
+    } 
+    catch (error) {
+        console.error("Error:", error)
+    }
+}
+
+getGithubProjects()
+getLanyard()
